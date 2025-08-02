c9c41717345aa2c036087a6bfbce56623b018f64
llghtdark <lcfilho19@gmail.com>
2025-08-02 16:25:16 -0300

    oops

--- youtubeapi.js
+++ youtubeapi.js
@@ -33,7 +33,8 @@ function onPlayerStateChange(event) {
      if (event.data === YT.PlayerState.ENDED) {
         playNextVideo();
     }
-    
+
+
     if (event.data === YT.PlayerState.PAUSED && soundToggle.checked && player.getCurrentTime() > 1) {
         pauseSound.play();
     }else if ((event.data === YT.PlayerState.BUFFERING || event.data === YT.PlayerState.UNSTARTED) && player.getCurrentTime() > 1) {
@@ -46,6 +47,7 @@ function playNextVideo() {
     let videoId = player.getVideoData().video_id;
     let currentIndex = playlistVideos.findIndex(video => video.videoId === videoId);
 
+    let nextVideo = playlistVideos[currentIndex+1]?.videoId;
     if (nextVideo) {
     player.loadVideoById(nextVideo);
     player.playVideo();
